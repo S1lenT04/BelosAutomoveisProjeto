b9d4d328033d3a890efc6c68ff24524577fa7a69
Vinicius-Vaz29 <viniciusmorinavaz90@gmail.com>
2025-11-28 18:43:34 +0000

    Created main classes

--- BelosAutomoveisProjeto/obj/Debug/net7.0-windows/BelosAutomoveisProjeto.AssemblyInfo.cs
+++ BelosAutomoveisProjeto/obj/Debug/net7.0-windows/BelosAutomoveisProjeto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BelosAutomoveisProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49822821908bf6ff0d65a310001b68b1a9878ee1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BelosAutomoveisProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BelosAutomoveisProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
